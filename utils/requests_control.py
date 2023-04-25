0f7a8fb62dc4cd64af5d7d3362c4bd0ff85daa8c
谈林海 <tanlinhai@tanlinhaideMacBook-Pro.local>
2023-04-25 16:05:25 +0800

    优化requests_control

--- utils/requests_control.py
+++ utils/requests_control.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import urllib3
+from urllib3.util.retry import Retry
 import requests
-from utils.log_control import logger
+from requests.adapters import HTTPAdapter
+from requests.exceptions import RetryError, Timeout, RequestException
 from jsonpath import jsonpath
 
-from utils.decorator_control import Log
 from utils import config
+from utils.log_control import logger
+from utils.decorator_control import Log
 from utils.create_cookie_control import Cookies
 
 
@@ -38,9 +41,30 @@ session.cookies, token = Authentication().cookie_token
 class RestClient:
     """封装api请求类"""
 
-    def __init__(self):
+    def __init__(self,
+                 timeout=10,
+                 retry=3,
+                 backoff_factor=0.3,
+                 proxies=None
+                 ):
+        # 初始化函数，设置请求超时时间、重试次数、backoff因子（指定失败后下一次重试时间间隔的增长因子）、代理等参数
+        # 这四个参数都是默认设置的，用户可以根据需要进行更改
         urllib3.disable_warnings()
+        self.timeout = timeout  # 超时时间
+        self.proxies = proxies or {}  # 设置代理
         self.session = session  # 创建会话对象
+        self.session.proxies.update(self.proxies)
+
+        retry_strategy = Retry(
+            total=retry,
+            backoff_factor=backoff_factor,
+            status_forcelist=[429, 500, 502, 503, 504],
+        )
+
+        adapter = HTTPAdapter(max_retries=retry_strategy)
+        self.session.mount("https://", adapter)
+        self.session.mount("http://", adapter)
+        # 设置重试策略并将其与会话对象关联，以便每次请求失败时自动重试
 
     @Log(True)
     def request(self, env, **kwargs):
@@ -55,7 +79,11 @@ class RestClient:
             'delete': lambda: self.session.delete(url, **kwargs)
         }.get(request_method.lower(), False)()
 
-        return res
+        try:
+            return res
+        except (RequestException, Timeout, RetryError) as err:
+            logger.error(f'请求失败: {err}')
+            return None
 
     @staticmethod
     def headers():
