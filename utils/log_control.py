ad2df69a1981760a376eb0a0724e7a7d68d03f1d
谈林海 <tanlinhai@tanlinhaideMacBook-Pro.local>
2023-05-16 09:38:33 +0800

    update

--- utils/log_control.py
+++ utils/log_control.py
@@ -2,11 +2,9 @@
 # -*- coding: utf-8 -*-
 # @Time : 2023/3/28 11:24
 # @Author : 谈林海
-import json
 import time
 import loguru
 
-from utils.allure_control import ReportStyle
 from utils.path import root
 
 log_path = root / 'logs'
@@ -24,42 +22,3 @@ class Loggings:
 
 logger = Loggings()
 
-
-class Log:
-    """日志操作装饰器"""
-
-    def __init__(self, switch=True):
-        self._switch = switch
-
-    def __call__(self, func):
-        def wrapper(*args, **kwargs):
-            if self._switch:
-                res = func(*args, **kwargs)
-                url, method = args[1][:2]
-                headers, data, json_data = kwargs.get('headers'), kwargs.get('data'), kwargs.get('json')
-                response = res.text
-                try:
-                    resp = json.loads(response)
-                except json.decoder.JSONDecodeError:
-                    logger.info('请求返回结果为text类型')
-                    resp = res.status_code
-                print("\n===============================================================")
-                res_info = f"\n请求地址: {url}\n" \
-                           f"请求方法: {method.upper()}\n" \
-                           f"请求头: {headers}\n" \
-                           f"请求数据: {data or json_data}\n" \
-                           f"响应数据: {resp}\n" \
-                           f"响应耗时(ms): {float(round(res.elapsed.total_seconds() * 1000))}\n" \
-                           f"接口响应码: {res.status_code}\n"
-                logger.info(res_info)
-                ReportStyle.allure_step_no(f"请求地址: {url}")
-                ReportStyle.allure_step_no(f"请求方法: {method.upper()}")
-                ReportStyle.allure_step("请求头", headers)
-                ReportStyle.allure_step("请求数据", data or json_data)
-                ReportStyle.allure_step_no(f"接口响应码: {res.status_code}")
-                ReportStyle.allure_step_no(f"响应耗时(ms): {round(res.elapsed.total_seconds() * 1000)}")
-                ReportStyle.allure_step("响应数据", resp)
-                return res
-            return func(*args, **kwargs)
-
-        return wrapper
--- utils/requests_control.py
+++ utils/requests_control.py
@@ -2,16 +2,19 @@
 # -*- coding: utf-8 -*-
 # @Time : 2023/4/24 15:59
 # @Author : 谈林海
+import json
 import urllib3
 from urllib3.util.retry import Retry
 import requests
 from requests.adapters import HTTPAdapter
 from requests.exceptions import RetryError, Timeout, RequestException
+from functools import wraps
 
 from utils import config
 from utils.json_control import JsonHandler
-from utils.log_control import logger, Log
+from utils.log_control import logger
 from utils.create_cookie_control import Cookies
+from utils.allure_control import ReportStyle
 
 
 class Authentication:
@@ -66,7 +69,38 @@ class RestClient:
         self.session.mount("http://", adapter)
         # 设置重试策略并将其与会话对象关联，以便每次请求失败时自动重试
 
-    @Log(True)
+    def res_log(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            res = func(*args, **kwargs)
+            url, method = args[1][:2]
+            headers, data, json_data = kwargs.get('headers'), kwargs.get('data'), kwargs.get('json')
+            response = res.text
+            try:
+                resp = json.loads(response)
+            except json.decoder.JSONDecodeError:
+                logger.info('请求返回结果为text类型')
+                resp = res.status_code
+            print("\n===============================================================")
+            res_info = f"\n请求地址: {url}\n" \
+                       f"请求方法: {method.upper()}\n" \
+                       f"请求头: {headers}\n" \
+                       f"请求数据: {data or json_data}\n" \
+                       f"响应数据: {resp}\n" \
+                       f"响应耗时(ms): {float(round(res.elapsed.total_seconds() * 1000))}\n" \
+                       f"接口响应码: {res.status_code}\n"
+            logger.info(res_info)
+            ReportStyle.allure_step_no(f"请求地址: {url}")
+            ReportStyle.allure_step_no(f"请求方法: {method.upper()}")
+            ReportStyle.allure_step("请求头", headers)
+            ReportStyle.allure_step("请求数据", data or json_data)
+            ReportStyle.allure_step_no(f"接口响应码: {res.status_code}")
+            ReportStyle.allure_step_no(f"响应耗时(ms): {round(res.elapsed.total_seconds() * 1000)}")
+            ReportStyle.allure_step("响应数据", resp)
+            return res
+        return wrapper
+
+    @res_log
     def request(self, env, **kwargs):
         url, request_method = env
         res = {
@@ -94,4 +128,5 @@ class RestClient:
         }
         return headers
 
+    res_log = staticmethod(res_log)
 
