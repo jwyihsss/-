db8e9ef2e4160437d0db4d92efcb8e0d50d50103
谈林海 <tanlinhai@tanlinhaideMacBook-Pro.local>
2023-07-11 15:08:33 +0800

    修改requests库为httpx

--- utils/commons/singleton_control.py
+++ utils/commons/singleton_control.py
@@ -2,18 +2,20 @@
 # -*- coding: utf-8 -*-
 # @Time : 2023/4/27 09:51
 # @Author : 谈林海
-from functools import lru_cache
+from functools import lru_cache, wraps
 
 
 def singleton(cls):
     """单例模式装饰器"""
+
     instances = {}
 
     @lru_cache(maxsize=None)
-    def get_instance(*args, **kwargs):
-        key = (cls, args, tuple(sorted(kwargs.items())))
+    @wraps(cls)
+    def wrapper(*args, **kwargs):
+        key = (cls, args, frozenset(kwargs.items()))
         if key not in instances:
             instances[key] = cls(*args, **kwargs)
         return instances[key]
 
-    return get_instance
+    return wrapper
